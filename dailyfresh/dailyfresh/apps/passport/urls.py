123974e35096090912f0b4fb38466ceedcef0b38
dongyangxian <huazhilian19@foxmail.com>
2018-10-09 15:56:58 +0800

    注册-发送短信验证码

--- dailyfresh/dailyfresh/apps/passport/urls.py
+++ dailyfresh/dailyfresh/apps/passport/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     # 每个路由信息都需要使用url函数来构造
     # url(路径, 视图)
     url(r'^imagecodes/(?P<image_id>\S+)/$', views.SendImageIdView.as_view()),
+    url(r'^smscodes/(?P<mobile>\d+)/$', views.SendSMSView.as_view()),
+
 ]
--- dailyfresh/dailyfresh/apps/passport/views.py
+++ dailyfresh/dailyfresh/apps/passport/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -30,3 +32,42 @@ class SendImageIdView(APIView):
             return Response({'message': '服务器内部错误'}, status=status.HTTP_507_INSUFFICIENT_STORAGE)
 
         return HttpResponse(image)
+
+class SendSMSView(APIView):
+    """发送短信验证码"""
+    def get(self, request, mobile):
+
+        # 1. 获取前端参数
+        text = request.GET.get('text')
+        image_code_id = request.GET.get('image_code_id')
+
+        if not image_code_id:
+            return Response({"message": "image_code_id is NULL"})
+
+        # 2. 判断图片验证码是否正确
+        conn = get_redis_connection('session')
+
+        # 2.1 取出验证码(取出的验证码为byte类型，所以需要解码)
+        image_code = conn.get('Code_%s' % image_code_id).decode()
+
+        if not image_code:
+            return Response({"message": "图片已过期"})
+
+        # 2.2 将取出的图片内容与输入的对比
+        if text.lower() != image_code.lower():
+            return Response({"message": "图片内验证码输入错误"})
+
+        # 2.3 如果对比成功，就将其删除
+        conn.delete('Code_%s' % image_code_id)
+
+        # 3. 生成短信验证码
+        sms_code = random.randint(0, 99999)
+        sms_code = '%06d' % sms_code
+        print(sms_code)
+
+        # 3.1 保存短信验证码
+        conn.setex("sms_code_%s" % mobile, 300, sms_code)
+        conn.setex("sms_flag_%s" % mobile, 60, 1)
+
+        # 4. 返回结果
+        return Response({"message": "OK"})
